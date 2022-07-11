9f8a4debe1b30c7024ad9ab86a8a3ae970ed3d9b
marce <marcelo.mc999@gmail.com>
2022-07-11 20:21:39 -0300

    Finished!

--- lib/bookings/booking.ex
+++ lib/bookings/booking.ex
@@ -1,9 +1,22 @@
 defmodule Flightex.Bookings.Booking do
-  @keys [:complete_date, :local_origin, :local_destination, :user_id, :id]
+  @keys [:id, :complete_date, :local_origin, :local_destination, :user_id]
   @enforce_keys @keys
+
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(complete_date, local_origin, local_destination, user_id) do
+    booking = %__MODULE__{
+      id: UUID.uuid4(),
+      complete_date: complete_date,
+      local_origin: local_origin,
+      local_destination: local_destination,
+      user_id: user_id
+    }
+
+    {:ok, booking}
   end
+
+  # def build(_local_origin, _local_destination, _complete_date, _user_id) do
+  #   {:error, "Invalid parameters"}
+  # end
 end
--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,9 +1,26 @@
 defmodule Flightex.Users.User do
-  @keys [:name, :email, :cpf, :id]
+  @keys [:id, :name, :email, :cpf]
   @enforce_keys @keys
+
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(id, name, email, cpf) do
+    user = %__MODULE__{
+      id: id,
+      name: name,
+      email: email,
+      cpf: cpf
+    }
+
+    {:ok, user}
   end
+
+  def build(name, email, cpf) when is_bitstring(cpf) do
+    id = UUID.uuid4()
+
+    build(id, name, email, cpf)
+  end
+
+  def build(_name, _email, cpf) when is_integer(cpf), do: {:error, "Cpf must be a String"}
+  def build(_name, _email, _cpf), do: {:error, "Invalid Parameters"}
 end
